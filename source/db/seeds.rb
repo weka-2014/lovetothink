067d05db01fb8d1dcf6b33121849d3bf74855973
euglazer <eugenestapler@gmail.com>
2014-12-12 11:57:05 +1300

    configured twitter gem, generated seeds file

--- source/db/seeds.rb
+++ source/db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+users = ["katyperry","justinbieber","BarackObama","taylorswift13","ladygaga","britneyspears","jtimberlake","rihanna","auria","Cristano","JLo","shakira","KimKardashian","Oprah","ddlovato","Pink","selenagomez","Randy_Gage","Harry_Styles","ArianaGrande","KAKA","onedirection","OfficialAdele","NiallOfficial","aliciakeys","BrunoMars","LilTunechi","venelibertario","Eminem","MileyCyrus","BillGates","pitbull","Real_Liam_Payne","NICKIMINAJ","Louis_Tomlinson","Drake","jimmyfallon","AvrilLavigne","aplusk","KingJames","wizkhalifa","davidguetta","neymarjr","EmWatson","MariahCarey","KevinHart4real","chrisbrown","Beyonce","zaynmalik","xtina","JimCarrey","ConanOBrien","kourtneykardash","ParisHilton","RyanSeacrest","danieltosh","iamwill","ashleytisdale","thomaspower","SrBachchan","ActuallyNPH","agnezmo","tyrabanks","AlejandroSanz","SnoopDogg","0xF2","khloekardashian","LeoDiCaprio","ivetesangalo","charliesheen","edsheeran","ricky_martin","10Ronaldinho","kanyewest","SimonCowell","carlyraejepsen","novaspivack","MohamadAlarefe","iamsrk","WayneRooney","iamdiddy","andresiniesta8","aamir_khan","ClaudiaLeitte","Usher","ZacEfron","gqindia"]
+
+users.each do |user|
+	meow = User.create!(email: Faker::Internet.email, password: "password",twitter_username: user)
+end
