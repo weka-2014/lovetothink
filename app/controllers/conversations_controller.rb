23ef73e3db8ab6fdeb3dab9de847ab87e882e08a
euglazer <eugenestapler@gmail.com>
2014-12-16 00:00:41 +1300

    OMFG CONVERSATION CREATE AND SHOW ACTIONS WORK OMGOMGOMGOMGOMGOGMOGMOGM

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,12 +1,29 @@
-class ConversationsController 
+class ConversationsController < ApplicationController
 
 	def index
 	end
 
 	def show
+		conversation = Conversation.find(params[:id])
+		user_1_id = conversation.user_1_id
+		user_2_id = conversation.user_2_id
+		if current_user.id == user_1_id || current_user.id == user_2_id
+			@users = conversation.formatted_users(current_user.id)
+			@messages = conversation.formatted_messages
+		else
+			redirect_to "/personal"
+		end
 	end
 
 	def create
+		user_1_id = params[:user_1_id]
+		user_2_id = params[:user_2_id]
+		if conversation = Conversation.find_by(user_1_id: user_1_id, user_2_id: user_2_id)
+			redirect_to "/conversations/#{conversation.id}"
+		else 
+			conversation = Conversation.create(user_1_id: user_1_id, user_2_id: user_2_id)
+			redirect_to "/conversations/#{conversation.id}"
+		end
 	end
 
 	def add_message
--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,4 +1,18 @@
 class Conversation < ActiveRecord::Base
 	has_many :messages, dependent: :destroy
+
+	def formatted_messages
+		messages.order(id: :asc).map do |message|
+			{ sender: User.find(message.sender_id), content: message.content }
+		end
+	end
+
+	def formatted_users(current_user_id)
+		if current_user_id == user_1_id
+			{ sender: User.find(user_1_id), recipient: User.find(user_2_id) }
+		else
+			{ sender: User.find(user_2_id), recipient: User.find(user_1_id) }
+		end
+	end
 	
 end
